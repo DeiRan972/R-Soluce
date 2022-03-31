e71a181b98d64e41c618d558eb695543acb4672a
dylan dol <dylan.dol@orange.fr>
2022-03-31 16:23:18 -0400

    add user tasks n keep same sidebar

--- Gemfile
+++ Gemfile
@@ -33,7 +33,6 @@ gem "simple_form"
 # user gem
 gem 'devise'
 
-
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,5 +6,4 @@ class Project < ApplicationRecord
   validates :description, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-
 end
