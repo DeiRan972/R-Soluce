5e281263c5b1a394a9101ad281a2697e54f279ff
natachaparnasse <natacha_parnasse@yahoo.fr>
2022-04-01 13:00:51 -0400

    update db with form

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,12 @@
 class Task < ApplicationRecord
+  ETAT = [ "In progress", "Approved", "In Review" ]
+
   belongs_to :project
   belongs_to :user
   validates :name, presence: true
   validates :description, presence: true
   validates :estimate_time, presence: true, numericality: { only_integer: true }
+  validates :status, presence: true, inclusion: { in: ETAT }
 
   def estimate_cost
     estimate_time * tjm
---
+++ db/migrate/20220401164204_change_column_status_to_string.rb
@@ -0,0 +1,5 @@
+class ChangeColumnStatusToString < ActiveRecord::Migration[6.1]
+  def change
+    change_column :tasks, :status, :string
+  end
+end
